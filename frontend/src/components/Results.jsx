bd324b9ad28564d47ae36b490c968b8ad7d46311
Siddhant Kodolkar <kodolkar.siddhant19@gmail.com>
2024-02-09 10:21:09 +0530

    designed results page

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Results = () => {
   const { powValues, prediction } = useSelector((state) => state.bci);
@@ -38,29 +39,50 @@ const Results = () => {
           </p>
         </div>
       ) : (
-        <div>
-          <h1>Results</h1>
-          <p>POW.F3.Theta: {powValues.pow_f3_theta}</p>
-          <p>POW.F3.BetaL: {powValues.pow_f3_beta_l}</p>
-          <p>POW.F4.Theta: {powValues.pow_f4_theta}</p>
-          <p>POW.F4.BetaL: {powValues.pow_f4_beta_l}</p>
-          <p>Predicted Attention Span: {prediction}</p>
-          <div className="scale-x-[-1] rotate-180 flex justify-evenly border border-red-400 bg-gray-300">
-            <div
-              className={`relative w-40 bg-red-500 transition-all duration-1000 ${
-                isTransitioned ? "h-32 bottom-0" : "h-1 bottom-30"
-              }`}
-            ></div>
-            <div
-              className={`relative w-40 bg-yellow-500 transition-all duration-1000 ${
-                isTransitioned ? "h-56 bottom-0" : "h-1 bottom-30"
-              }`}
-            ></div>
-            <div
-              className={`relative w-40 bg-green-500 transition-all duration-1000 ${
-                isTransitioned ? "h-80 bottom-0" : "h-1 bottom-30"
-              }`}
-            ></div>
+        <div className="flex">
+          <div className="bg-black text-white p-4 h-screen w-56 ">
+            <p className="text-lg cursor-pointer p-1 mb-1">
+              <Link to="/">Home</Link>
+            </p>
+            <p className="text-lg cursor-pointer bg-slate-800 rounded p-1">
+              Results
+            </p>
+          </div>
+          <div className="w-full bg-gray-300">
+            <h1 className="text-center text-2xl font-bold m-8">Results</h1>
+            <div className="flex justify-around ">
+              <p className="font-bold">POW.F3.Theta</p>
+              <p className="font-bold">POW.F3.BetaL</p>
+              <p className="font-bold">POW.F4.Theta</p>
+              <p className="font-bold">POW.F4.BetaL</p>
+            </div>
+            <div className="flex justify-around mb-14">
+              <p>{powValues.pow_f3_theta}</p>
+              <p>{powValues.pow_f3_beta_l}</p>
+              <p>{powValues.pow_f4_theta}</p>
+              <p>{powValues.pow_f4_beta_l}</p>
+            </div>
+            <div className="scale-x-[-1] rotate-180 border-blue-400 border-double border-2 flex justify-evenly bg-gray-300">
+              <div
+                className={`relative w-40 bg-red-500 transition-all duration-1000 ${
+                  isTransitioned ? "h-32 bottom-0" : "h-1 bottom-30"
+                }`}
+              ></div>
+              <div
+                className={`relative w-40 bg-yellow-500 transition-all duration-1000 ${
+                  isTransitioned ? "h-56 bottom-0" : "h-1 bottom-30"
+                }`}
+              ></div>
+              <div
+                className={`relative w-40 bg-green-500 transition-all duration-1000 ${
+                  isTransitioned ? "h-80 bottom-0" : "h-1 bottom-30"
+                }`}
+              ></div>
+            </div>
+            <p className="text-center">
+              Predicted Attention Span: {prediction}
+            </p>
+            Inference
           </div>
         </div>
       )}
