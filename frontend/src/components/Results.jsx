b4063563916aa167bd9e2ac54b5ae7ea68c0bf9e
Siddhant Kodolkar <kodolkar.siddhant19@gmail.com>
2024-02-09 22:25:50 +0530

    added visualisation and legend

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -26,12 +26,36 @@ const Results = () => {
     };
   }, []); // Empty dependency array ensures the effect runs only once
 
+  const getBarProperties = () => {
+    switch (prediction) {
+      case "Low attention":
+        return {
+          color: "red",
+          height: isTransitioned ? "h-32 bottom-0" : "h-1 bottom-30",
+        };
+      case "Moderate attention":
+        return {
+          color: "yellow",
+          height: isTransitioned ? "h-56 bottom-0" : "h-1 bottom-30",
+        };
+      case "High attention":
+        return {
+          color: "green",
+          height: isTransitioned ? "h-80 bottom-0" : "h-1 bottom-30",
+        };
+      default:
+        return {};
+    }
+  };
+
+  const barProperties = getBarProperties();
+
   return (
     <div>
       {isLoading ? (
         <div>
           <div className="mt-60 flex items-center justify-center">
-            <div className="border-t-4 border-blue-500 border-solid w-16 h-16 border-r-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+            <div className="border-t-4 border-blue-500 border-solid w-16 h-16 border-r  rounded-full animate-spin"></div>
           </div>
 
           <p id="res" className="text-center text-3xl font-bold mt-4">
@@ -41,10 +65,10 @@ const Results = () => {
       ) : (
         <div className="flex">
           <div className="bg-black text-white p-4 h-screen w-56 ">
-            <p className="text-lg cursor-pointer p-1 mb-1">
+            <p className="text-lg font-serif cursor-pointer p-1 mb-1">
               <Link to="/">Home</Link>
             </p>
-            <p className="text-lg cursor-pointer bg-slate-800 rounded p-1">
+            <p className="text-lg font-serif cursor-pointer bg-slate-800 rounded p-1">
               Results
             </p>
           </div>
@@ -62,27 +86,34 @@ const Results = () => {
               <p>{powValues.pow_f4_theta}</p>
               <p>{powValues.pow_f4_beta_l}</p>
             </div>
-            <div className="scale-x-[-1] rotate-180 border-blue-400 border-double border-2 flex justify-evenly bg-gray-300">
-              <div
-                className={`relative w-40 bg-red-500 transition-all duration-1000 ${
-                  isTransitioned ? "h-32 bottom-0" : "h-1 bottom-30"
-                }`}
-              ></div>
-              <div
-                className={`relative w-40 bg-yellow-500 transition-all duration-1000 ${
-                  isTransitioned ? "h-56 bottom-0" : "h-1 bottom-30"
-                }`}
-              ></div>
-              <div
-                className={`relative w-40 bg-green-500 transition-all duration-1000 ${
-                  isTransitioned ? "h-80 bottom-0" : "h-1 bottom-30"
-                }`}
-              ></div>
+            <div className="flex flex-row border border-blue-500">
+              <div className="w-1/2 border border-gray-500 flex flex-row justify-between">
+                <div
+                  className={`relative border-2 rounded border-black ml-4 w-44 bg-${barProperties.color}-500 transition-all duration-1000 ${barProperties.height}`}
+                ></div>
+
+                <div className="bg-white inline-block rounded-xl p-3 h-32 mr-4 mt-4">
+                  <div class="flex items-center justify-between">
+                    <div class="pr-2 font-light">Low</div>
+                    <div class=" w-8 h-1 bg-red-500"></div>
+                  </div>
+                  <div class="flex items-center mt-3 justify-between">
+                    <div class="pr-2 font-light">Medium</div>
+                    <div class="w-8 h-1 bg-yellow-500"></div>
+                  </div>
+                  <div class="flex items-center mt-3 justify-between">
+                    <div class="pr-2 font-light">High</div>
+                    <div class="w-8 h-1 bg-green-500"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <p className="text-center">
+                  Predicted Attention Span:  {prediction}
+                </p>
+              </div>
             </div>
-            <p className="text-center">
-              Predicted Attention Span: {prediction}
-            </p>
-            Inference
+            <p className="ml-6 font-serif">Level of attention span</p>
           </div>
         </div>
       )}
